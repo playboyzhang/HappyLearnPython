5a803e92d035788fcdbe266059e4881fb692aee3
boy <sillystick@126.com>
2019-12-12 11:06:35 +0800

    添加类stack

--- PythonDataStructure/baseConverter.py
+++ PythonDataStructure/baseConverter.py
@@ -1,4 +1,21 @@
-from pythonds.basic.stack import Stack
+class Stack:
+     def __init__(self):
+         self.items = []
+
+     def isEmpty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
 
 def baseConverter(decNumber,base):
     digits = "0123456789ABCDEF"
